86e08315934d0de34326789e81e9756326c63d6f
alex <aboguszewski2037@stu.d214.org>
2019-11-06 11:19:10 -0600

    Initial Commit

---
+++ intergalacticTravle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  intergalacticTravle
+//
+//  Created by alex boguszewski on 11/6/19.
+//  Copyright © 2019 alex boguszewski. All rights reserved.
+// Checking
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
